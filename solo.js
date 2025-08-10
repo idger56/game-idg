0a7fba8cc6c9d45111a5d064e53b2a1fb2628da0
idger56 <101880291+idger56@users.noreply.github.com>
2025-08-10 21:57:53 +0300

    Add files via upload

--- solo.js
+++ solo.js
@@ -9,7 +9,7 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 
-import { auth, db } from "./firebase-config.js";
+import { db } from "./firebase-config.js";
 
 import {
   getFirestore,
